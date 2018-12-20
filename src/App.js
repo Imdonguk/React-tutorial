9f71603d997e350e9050ec8701f21a8f92ccd9c1
Imdonguk <com8157@naver.com>
2018-12-20 20:01:41 +0900

    Add : phone정보를 추가하고, 수정하고, 제거하는 핸들러함수 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 import PhoneForm from './components/PhoneForm'
+import PhoneList from './components/PhoneList'
+
 class App extends Component {
+
+  state = {
+    information: []
+  }
+
+  index = 0;
+
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      "information": information.filter(v => v.id !== id)
+    });
+  }
+
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      "information": information.concat(Object.assign({}, data, { id: this.index++ }))
+    })
+  }
+
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      'information': information.map(v => {
+        if (v.id === id) return Object.assign({}, { id: id }, data);
+        return v;
+      })
+    })
+  }
+
+
   render() {
+    const { information } = this.state;
     return (
       <div className="App">
-        <PhoneForm />
+        <PhoneForm onCreate={this.handleCreate} />
+        <PhoneList
+          info={information}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
+        />
       </div>
     );
   }
